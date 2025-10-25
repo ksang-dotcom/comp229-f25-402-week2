f284c0fb3d8f8a6fa29d14259accf80ea04af5cf
Kalsang Chodon <kchodon1@my.centennialcollege.ca>
2025-10-25 15:15:05 -0400

    Fix Render Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'comp229-f25-402-week2-19o1.onrender.com'
+    ]
+  } 
 })
